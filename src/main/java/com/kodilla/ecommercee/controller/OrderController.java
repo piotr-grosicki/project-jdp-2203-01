129b76c193e4d499d85290d621028082845ee8b5
Michal <kudykmichal@gmail.com>
2022-02-28 21:18:09 +0100

    changing paths

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/v1/orders")
+@RequestMapping("/v1")
 public class OrderController {
     private final OrderDto orderDto = OrderDto.builder()
                                               .id(1L)
@@ -21,58 +21,29 @@ public class OrderController {
                                               .creationDate(LocalDate.now())
                                               .orderConfirmation(true)
                                               .build();
-/*
-    private final DbService service;
-    private final OrderMapper mapper;
-
-    public OrderController(DbService service, OrderMapper mapper) {
-        this.service = service;
-        this.mapper = mapper;
-    }
-*/
 
     @RequestMapping(method = RequestMethod.GET, value = "/orders")
     public ResponseEntity<List<OrderDto>> getOrders() {
-        /*
-        List<Order> orders = service.getOrders();
-        return new ResponseEntity<>(mapper.toDtoList(orders), HttpStatus.OK);
-        */
         return new ResponseEntity<>(Stream.of(orderDto).collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/order/{orderId}")
     public ResponseEntity<OrderDto> getOrder(@PathVariable Long orderId) {
-        /*
-        Order order = service.getOrderById(orderId);
-        return new ResponseEntity<>(mapper.toOrderDto(order), HttpStatus.OK);
-        */
         return new ResponseEntity<>(orderDto, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/order", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OrderDto> addOrder(@RequestBody OrderDto newOrderDto) {
-        /*
-        Order order = mapper.toOrderDto(newOrderDto);
-        OrderDto savedOrderDto = service.saveOrder(order);
-        return new new ResponseEntity<>(savedOrderDto, HttpStatus.OK);
-        */
         return new ResponseEntity<>(orderDto, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/order")
     public ResponseEntity<Void> updateOrder(@RequestBody OrderDto updatedOrderDto) {
-        /*
-        Order order = mapper.toOrderDto(updatedOrderDto);
-        service.saveOrder(order);
-        */
         return ResponseEntity.ok().build();
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/order/{orderId}")
     public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
-        /*
-            service.removeOrder(orderId);
-        */
         return ResponseEntity.ok().build();
     }
 }
