c704fe49168b94871c2a691c8ee25df97e582bbd
Katarzyna Nabożny <knnabozny@gmail.com>
2022-03-01 21:47:01 +0100

    JDP220301-1: Remove comments

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.kodilla.ecommercee.controller;
+
 import com.kodilla.ecommercee.domain.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,51 +15,37 @@ import java.util.List;
 @RequestMapping("/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
-    private ProductDto bagStub = new ProductDto("bag", 1L, BigDecimal.TEN,true,"Red and hot");
-
-    // private final DbService service;
-    //  private final ProductMapper productMapper;
-
+    private ProductDto bagStub = new ProductDto("bag", 1L, BigDecimal.TEN,
+            true, "Red and hot");
 
     @GetMapping
     public ResponseEntity<List<ProductDto>> getProducts() {
-        //  List<Product> products = service.getAllProducts();
-        //  return ResponseEntity.ok( productMapper.mapToProductDtoList(products));
         return ResponseEntity.ok(getListOfProducts());
     }
 
     private List<ProductDto> getListOfProducts() {
         List<ProductDto> productList = new ArrayList<>();
-
         productList.add(bagStub);
         return productList;
     }
 
-
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createProduct(@RequestBody ProductDto productDto) {
-        // Product product = productMapper.mapToProduct(productDto);
-        //  service.saveProduct(product);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping(value = "/{productId}")
-    public ResponseEntity<ProductDto> getProduct(@PathVariable Long productId) { //throws ProductNotFoundException {
-        // return ResponseEntity.ok(productMapper.mapToProductDto(service.getProduct(productId)));
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long productId) {
         return ResponseEntity.ok(bagStub);
     }
 
     @PutMapping
     public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto productDto) {
-        // Product product = productMapper.mapToProduct(productDto);
-        //  Product savedProduct = service.saveProduct(product);
-        // return ResponseEntity.ok(productMapper.mapToProductDto(savedProduct));
         return ResponseEntity.ok(bagStub);
     }
 
     @DeleteMapping(value = "/{productId}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {//throws ProductNotFoundException {
-        // service.deleteProduct(productId);
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
         return ResponseEntity.ok().build();
     }
 
