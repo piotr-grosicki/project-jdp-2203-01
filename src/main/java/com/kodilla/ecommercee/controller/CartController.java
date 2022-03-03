c2101786083fdea8a0678f7297e0cb0868fbf400
Jakub Kirker <jakubkir@wp.pl>
2022-03-03 19:29:09 +0100

    JDP220301-3

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,32 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.CartDto;
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartController {
+    @GetMapping("/{idCart}")
+    public ResponseEntity<List<ProductDto>> getProductsFromCart(@PathVariable Long idCart) throws CartNotFoundsExpections {
+        return ResponseEntity.ok(new ArrayList<>());
+    }
+
+
+    @PutMapping("addProduct/{idCart}/{idProduct}")
+    public ResponseEntity<CartDto> addProductToCart(@PathVariable Long idCart, @PathVariable Long idProduct) throws CartNotFoundsExpections {
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PutMapping("/{idCart}/{idProduct}")
+    public ResponseEntity<CartDto> deleteProductFromCart(@PathVariable Long idCart, @PathVariable Long idProduct) throws CartNotFoundsExpections {
+        return ResponseEntity.ok().build();
+    }
 }
+
+
---
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -0,0 +1,18 @@
+package com.kodilla.ecommercee.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class CartDto {
+    private Long id;
+    private List<Long> productIds;
+}
