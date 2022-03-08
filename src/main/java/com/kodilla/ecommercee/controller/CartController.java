22edd65dcfa6c991bcc58fb7aef26306e4ebbe2a
Jakub Kirker <jakubkir@wp.pl>
2022-03-08 18:26:07 +0100

    JDP220301-3

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -2,6 +2,7 @@ package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.CartDto;
 import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +18,7 @@ public class CartController {
     }
 
 
-    @PostMapping("addProduct/{idCart}/{idProduct}")
+    @PostMapping("/{idCart}/{idProduct}")
     public ResponseEntity<CartDto> addProductToCart(@PathVariable Long idCart, @PathVariable Long idProduct) {
         return ResponseEntity.ok().build();
     }
@@ -27,6 +28,18 @@ public class CartController {
     public ResponseEntity<CartDto> deleteProductFromCart(@PathVariable Long idCart, @PathVariable Long idProduct) {
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createOrderFromCart(@PathVariable Long OrderID) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createEmptyCart(@PathVariable Long idCart) {
+        return ResponseEntity.ok().build();
+    }
+
+
 }
 
 
