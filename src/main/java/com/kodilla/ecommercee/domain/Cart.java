425ec3c36f4d9043b3648e574126292a63d826e7
RobertSliwinski <23robert@wp.pl>
2022-03-10 23:43:53 +0100

    zamieniłem @OneToMany na @OneToOne w relacji pomiędzy tabelami Cart i Order

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -26,13 +26,9 @@ public class Cart {
     @JoinColumn(name = "USER_ID")
     private User user;
 
-    @OneToMany(
-            targetEntity = Order.class,
-            mappedBy = "id",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY
-    )
-    private List<Order> order = new ArrayList<>();
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
 
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
