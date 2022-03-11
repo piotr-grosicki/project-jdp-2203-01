472e93888c18dc163636cdf81ab3e262804b37f5
Jakub Kirker <jakubkir@wp.pl>
2022-03-11 21:25:49 +0100

    JDP220301-16P

--- src/test/java/com/kodilla/ecommercee/domain/OrderTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/OrderTestSuite.java
@@ -17,11 +17,11 @@ import java.time.LocalDate;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderTestSuite {
-
 
 
     @Autowired
@@ -35,22 +35,16 @@ public class OrderTestSuite {
     private Order order1;
     private Order order2;
 
-    @Before
-    public void userAndOrder(){
-        User user1 = new User();
-        Order order1 = new Order();
-        Order order2 = new Order();
-
-    }
-
-
 
     @Test
-    public void testGetsAllOrders(){
+    public void testGetsAllOrders() {
 
         //Given
-
+        Order order1 = new Order();
+        Order order2 = new Order();
         //When
+        orderRespository.save(order1);
+        orderRespository.save(order2);
         int result = orderRespository.findAll().size();
 
         //Then
@@ -60,14 +54,9 @@ public class OrderTestSuite {
     }
 
     @Test
-    public void testAddNewOrder(){
+    public void testAddNewOrder() {
         //Given
-        Order order = new Order(
-                LocalDate.now().minusDays(3),
-                LocalDate.now().plusDays(7),
-                user,
-                new Cart(user)
-        );
+        Order order = new Order();
         //When
         orderRespository.save(order);
         int result = orderRespository.findAll().size();
@@ -76,23 +65,37 @@ public class OrderTestSuite {
     }
 
     @Test
-    public void testUpdateOrder() {
+    public void testDeleteOrder() {
         //Given
-        Order order = new Order(
-                LocalDate.now().minusDays(3),
-                LocalDate.now().plusDays(7),
-                user,
-                new Cart(user)
-        );
+
+        Order order = new Order();
 
         orderRespository.save(order);
-        long orderID = order.getId();
+        long orderId = order.getId();
 
         //When
-        Order updateOrder = orderRespository.findById(orderID).get();
-        orderRespository.save(updateOrder);
+        orderRespository.deleteById(orderId);
+
         //Then
-        assertEquals(orderRespository.findAllById(orderID));
+        assertEquals(3, orderRespository.findAll().size());
+        assertFalse(orderRespository.existsById(orderId));
+        assertEquals(3, orderRespository.findAll().size());
+
     }
 
+    @Test
+    public void testUpdateOrder() {
+        //Given
+        Order order = new Order();
+        orderRespository.save(order);
+        long orderId = order.getId();
+
+        //When
+        Order updatedOrder = orderRespository.findById(orderId).get();
+        updatedOrder.getId();
+        orderRespository.save(updatedOrder);
+
+        //Then
+        assertEquals(3, orderRespository.findById(orderId).get());
+    }
 }
